e595b623086fd1df119f762547620947d4adf59a
zeevosec <8338013+zeevosec@users.noreply.github.com>
2020-08-26 22:17:27 -0400

    move channels up

--- src/jobs/collectPosts.js
+++ src/jobs/collectPosts.js
@@ -18,48 +18,48 @@ class CollectPostsJob extends Job {
   async run() {
     this.log('Collecting posts...');
     const { Post } = this.database;
-    this.client.channels.cache
-      .filter((channel) => {
-        return channel.type === 'text' && channel.name.toLowerCase() !== 'general';
-      })
-      .forEach(async (chl) => {
-        const channel = await chl.fetch();
-        const messages = await channel.messages.fetch({ limit: 100 });
-        const content = messages.filter(isValidMessage);
-        content.forEach(async (ct) => {
-          const { author } = ct;
-          const post = await Post.findAll({
-            where: {
-              [Op.or]: [
-                {
-                  discordId: {
-                    [Op.eq]: ct.id,
-                  },
+    const channels = this.client.channels.cache.filter((channel) => {
+      return channel.type === 'text' && channel.name.toLowerCase() !== 'general';
+    });
+
+    channels.forEach(async (chl) => {
+      const channel = await chl.fetch();
+      const messages = await channel.messages.fetch({ limit: 100 });
+      const content = messages.filter(isValidMessage);
+      content.forEach(async (ct) => {
+        const { author } = ct;
+        const post = await Post.findAll({
+          where: {
+            [Op.or]: [
+              {
+                discordId: {
+                  [Op.eq]: ct.id,
                 },
-                {
-                  url: {
-                    [Op.eq]: ct.content,
-                  },
+              },
+              {
+                url: {
+                  [Op.eq]: ct.content,
                 },
-              ],
-            },
-          });
-          if (!post.length) {
-            this.log(`Found new message ${ct.content}`);
-            await Post.create({
-              content: ct.content,
-              url: ct.content,
-              discordId: ct.id,
-              authorDiscordUsername: author.username,
-              authorDiscordDiscriminator: author.discriminator,
-              authorDiscordId: author.id,
-              createdTimestamp: ct.createdTimestamp,
-              channelDiscordId: channel.id,
-              channelDiscordName: channel.name,
-            });
-          }
+              },
+            ],
+          },
         });
+        if (!post.length) {
+          this.log(`Found new message ${ct.content}`);
+          await Post.create({
+            content: ct.content,
+            url: ct.content,
+            discordId: ct.id,
+            authorDiscordUsername: author.username,
+            authorDiscordDiscriminator: author.discriminator,
+            authorDiscordId: author.id,
+            createdTimestamp: ct.createdTimestamp,
+            channelDiscordId: channel.id,
+            channelDiscordName: channel.name,
+          });
+        }
       });
+    });
   }
 }
 
